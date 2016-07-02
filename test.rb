2b472c33aeb74b8f55fab0fec5d960b5c7574fa6
masaru uozaki <sakana@uozakimasaru-no-MacBook-Pro.local>
2016-07-02 19:19:08 +0900

    コード追加

--- test.rb
+++ test.rb
@@ -1,44 +1,104 @@
 # -*- coding: utf-8 -*-
 require 'date'
 
-class Student
-	def initialize(name, age)
-		@name = name
-		@age = age
-	end
-	def to_s
-		"#{@name},#{@value}"
-	end
-end
+
+#ゲッター、名前　年齢を個別に表示できる
+#セッター、名前、年齢を変更できる
+#ゲッターがないとセッターが表示されたない
+#アクセスメソッドでゲッターセッターを簡単に定義できる
+# attr_accessor :name, :age
+
+#ハッシュ
+#個別の呼び出しかたは puts[:hoge]
+#要素すべての呼び出しかたは each { |key, value| puts "#{key} #{value}"}
+
+#配列
+#hoge = [fuga, zuga]
+#ハッシュ
+#hoge = { fuga: 'fuga', zuga: 'zuga'}
 
 
 
-class StudentBook
-	def initialize
-		@student = {}
-	end
 
-	def setUpStudent
-		@student = {
-			yamada: Student.new('山田', 18),
-			satou: Student.new('佐藤', 28),
-			suzuki: Student.new('鈴木', 33)
-		}
-	end
-	def printStudnet
-		@student.each { |key, value|
-			puts "#{key}: #{value}"
-		}
-	end
+# class Student
+# 	def initialize(name, age)
+# 		@name = name
+# 		@age = age
+# 	end
+# 	def to_s
+# 		"#{@name},#{@value}"
+# 	end
+# end
 
-	def listAllStudent
-		setUpStudent
-		printStudnet
-	end
-end
 
-studentBook = StudentBook.new
-studentBook.listAllStudent
+# class StudentBook
+# 	def initialize
+# 		@student = {}
+# 	end
+
+# 	def setUpStudent
+# 		@student = {
+# 			yamada: Student.new('山田', 18),
+# 			satou: Student.new('佐藤', 28),
+# 			suzuki: Student.new('鈴木', 33)
+# 		}
+# 	end
+# 	def printStudnet
+# 		@student.each { |key, value|
+# 			puts "#{key}: #{value}"
+# 		}
+# 	end
+
+# 	def listAllStudent
+# 		setUpStudent
+# 		printStudnet
+# 	end
+# end
+
+# studentBook = StudentBook.new
+# studentBook.listAllStudent
+
+
+# class OsakaToin
+# 	def initialize(name, age, position, number)
+# 		@name = name
+# 		@age = age
+# 		@position = position
+# 		@number = number
+# 	end
+# 	def to_s
+# 		"#{@name},#{@age},#{@position},#{@number}"
+# 	end
+# 	def toFormattedString()
+# 		"名前: #{@name}, 年齢: #{@age}, ポジション: #{@position}, 背番号: #{@number}"
+# 	end
+# end
+
+# class OsakaToinPlayer
+# 	def initialize
+# 		@students = {}
+# 	end
+# 	def setUpStudents
+# 		@students = {
+# 			player1: OsakaToin.new('根尾昴', 16, '投手', '18番'),
+# 			player2: OsakaToin.new('吉澤', 18, '三塁手', '5番')
+# 		}
+# 	end
+# 	def printStudents
+# 		@students.each { |key, value|
+# 			puts "#{key}, #{value.toFormattedString}"
+# 		}
+# 	end
+
+# 	def listAllStudents
+# 		setUpStudents
+# 		printStudents
+# 	end
+# end
+
+# osakaToinPlayer = OsakaToinPlayer.new()
+
+# osakaToinPlayer.listAllStudents
 
 
 
