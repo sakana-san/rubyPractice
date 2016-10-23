231e6da943f606b65708387fda611c5eed562c84
masaru uozaki <sakana@uozakimasaru-no-MacBook-Pro.local>
2016-10-23 13:52:43 +0900

    ジャンルを変えてリファクタリングもした！

--- testDbi.rb
+++ testDbi.rb
@@ -2,131 +2,129 @@
 require 'date'
 require 'dbi'
 
-class Hanabi
-	def initialize(name, desc)
+class OsakaToin
+	def initialize(name, position, grade)
 		@name = name
-		@desc = desc
+		@position = position
+		@grade = grade
 	end
-	attr_accessor :name, :desc
+	attr_accessor :name, :position, :grade
 	def to_s
-		"#{@name},#{@desc}"
-	end
-	def toFormatString(sep= "\n")
-		"名前:#{@name}#{sep}役柄:#{@desc}#{sep}"
+		"#{@name}, #{@position}, #{@grade}"
 	end
 end
 
-class HanabiCasting
+class OsakaToinPlayer
 	def initialize(sqlite)
 		@db = sqlite
 		@dbh = DBI.connect("DBI:SQLite3:#{@db}")
-		@item = {}
+		@player = {}
 	end
 	def init
-		puts "\n0. データの初期化"
+		print "\n 0.データの初期化"
 		print "初期化しますか？（Y/yなら初期化を実行します）"
+
 		answer = gets.chomp.upcase
 		if /^Y$/ =~ answer
-			#hanabiInfoがある場合は削除する
-			@dbh.do("drop table if exists hanabiInfo")
-			#hanabiInfoがない場合はテーブル作成
-			@dbh.do("create table hanabiInfo (
-				id    varchar(50)   not null,
-				name  varchar(100)  not null,
-				desc  varchar(100)  not null
+			@dbh.do("drop table if exists toinData")
+			@dbh.do("create table toinData(
+				id   varchar(50)  not null,
+				name varchar(100)  not null,
+				position  varchar(100)  not null,
+				grade  varchar(50)  not null
 			);")
 			puts "\n初期化しました"
 		end
 	end
-	def add
-		puts "\n1. データの登録"
+	def set
+		osakaToin = OsakaToin.new(" ", " ", " ")
+		print "\n 1.データの登録"
 		print "データを登録します"
-		hanabi = Hanabi.new("", "")
-		print "\n"
 
+		print "\n"
 		print "キー:"
 		key = gets.chomp
 
-		print "名前："
-		hanabi.name = gets.chomp
-		print "役柄："
-		hanabi.desc = gets.chomp
+		print "名前:"
+		osakaToin.name = gets.chomp
+		print "ポジション:"
+		osakaToin.position = gets.chomp
+		print "学年:"
+		osakaToin.grade = gets.chomp
 
-		flag = 0
-		#もし名前か役柄が入力されていたらflagに1を代入
-		flag = 1 if hanabi.name != '' || hanabi.desc != ''
-		#flagが1なら
-		if flag == 1
-			p flag
-			#hanabiInfoにターブルの項目を入力する
-			@dbh.do("insert into hanabiInfo values(
+		if osakaToin.name != '' && osakaToin.position != '' && osakaToin.grade != ''
+			@dbh.do("insert into toinData values(
 				\'#{key}\',
-				\'#{hanabi.name}\',
-				\'#{hanabi.desc}\'
-			);")
-			puts "\n登録しました"
+				\'#{osakaToin.name}\',
+				\'#{osakaToin.position}\',
+				\'#{osakaToin.grade}\'
+			)")
+			puts "入力しました"
 		else
-			print "入力してください"
+			puts "入力してください"
 		end
 	end
 	def list
 		counts = 0
 		#テーブルの項目を日本語に変えるハッシュ
-		@item = {id: 'キー', name: '名前:', desc: '役柄'}
-		puts "\n2. データの表示"
+		@item = {id: 'キー', name: '名前', position: '年齢'}
+		puts "\n データの表示"
 
 		#テーブルからデータを読み込んで表示
-		sth = @dbh.execute("select * from hanabiInfo")
+		sth = @dbh.execute('select * from toinData')
 		#select分を1件ずつrowに取り出し繰り返し処理
 		sth.each do |row|
 			puts "\n-----------------------------"
 			#each_with_nameメソッドで値と項目名を取り出す
 			row.each_with_name do |value, name|
-				#@itemで項目名を日本語に変換
-				puts "#{@item[name]}#{value.to_s}"
+				print "#{@item[name]} #{value.to_s}"
 			end
-			puts "\n-----------------------------"
+			puts "\n-------------------------"
 			counts += 1
 		end
 		#実行結果の解放
 		sth.finish
-		# 0件ならエラー文言それ以外なら件数表示
-		displayCount = (counts == 0) ? "\n 登録されていません" : "\n#{counts}件のデータを表示します"
-		print displayCount
+		# 0件ならエラー文言、それ以外なら件数表示
+		displayCount = (counts == 0) ? "\n登録されていません" : "\n#{counts}件のデータを表示します"
+		puts displayCount
 	end
 	def modify
-		puts "\n3. データの修正"
-		print "データを修正します。"
-		hanabi = Hanabi.new("", "")
+		print "\n3. データの修正"
+		print "データを修正します"
+
+		osakaToin = OsakaToin.new(" ", " ", " ")
 		print "\n"
 
 		print "キー:"
 		key = gets.chomp
 
-		print "名前："
-		hanabi.name = gets.chomp
-		print "役柄："
-		hanabi.desc = gets.chomp
+		print "名前:"
+		osakaToin.name = gets.chomp
+		print "ポジション:"
+		osakaToin.position = gets.chomp
+		print "学年:"
+		osakaToin.grade = gets.chomp
 
 		# レコードのデータを修正する
-		@dbh.do("update hanabiInfo
-			set id	= \'#{key}\',
-				  name	= \'#{hanabi.name}\',
-			    desc	= \'#{hanabi.desc}\';
+		@dbh.do("update toinData
+			set id = \'#{key}\',
+					name = \'#{osakaToin.name}\',
+					position = \'#{osakaToin.position}\',
+					grade = \'#{osakaToin.grade}\'
 		")
-		puts "\n修正しました"
+		puts "\n修正しますた"
 	end
 	def delete
-		puts "\n8. データの削除"
+		print "\n8. データの削除"
 		print "キー:"
 		key = gets.chomp
-		print "このレコードを削除しますか？（Y/yなら削除を実行します）："
+		print "このレコードを削除しますか？（Y/yなら削除を実行します):"
 
 		answer = gets.chomp.upcase
 		if /^Y$/ =~ answer
 			# keyと照合して合致すればそのレコードを削除する
-			@dbh.do("delete from hanabiInfo where id = \'#{key}\';")
-			puts "\n削除しました"
+			@dbh.do("delete from toinData where id = \'#{key}\';")
+			puts "\n削除しますた"
 		end
 	end
 	def run
@@ -144,7 +142,7 @@ class HanabiCasting
 				when "0" == num
 					init
 				when "1" == num
-					add
+					set
 				when "2" == num
 					list
 				when "3" == num
@@ -158,6 +156,8 @@ class HanabiCasting
 		end
 	end
 end
-hanabiCasting = HanabiCasting.new('hanabiData.db')
 
-hanabiCasting.run
+
+osakaToinPlayer = OsakaToinPlayer.new('toinData.db')
+
+puts osakaToinPlayer.run
