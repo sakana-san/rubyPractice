ec240c7a9172848276c6b46d52475c014a102055
masaru uozaki <sakana@uozakimasaru-no-MacBook-Pro.local>
2016-10-30 19:25:05 +0900

    search-resultのDOM,CSS,サーバープログラム実装

--- conf_erb.rb
+++ conf_erb.rb
@@ -17,6 +17,25 @@ server = WEBrick::HTTPServer.new( config )
 # erbのMIMEタイプを設定
 server.config[:MimeTypes]["erb"] = "text/html"
 
+# 一覧表示からの処理
+# "http://localhost:8099/list" で呼び出される
+server.mount_proc("/list") { |req, res|
+  p req.query
+  # 'operation'の値の後の（.delete, .edit）で処理を分岐する
+  if /(.*)\.(delete|edit)$/ =~ req.query['operation']
+    target_id = $1
+    oparation = $2
+    # 選択された処理を実行する画面に移行する
+    # ERBを、ERBHandlerを経由せずに直接呼び出して利用している
+    template = ERB.new(File.read('delete.erb')) if operation == 'delete'
+    template = ERB.new(File.read('edit.erb')) if operation == 'edit'
+    res.body << template.result(building)
+  else #データが選択されていないなど
+    template = ERB.new( File.read('no_selected.erb') )
+    res.body << template.result( binding )
+  end
+}
+
 # Ctrl-C割り込みがあった場合にサーバーを停止する処理を登録しておく
 trap(:INT) do
   server.shutdown
