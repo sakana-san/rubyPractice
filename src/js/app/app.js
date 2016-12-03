14db6a6cc2fbaaf1b774099295cdd6d653179116
masaru uozaki <sakana@uozakimasaru-no-MacBook-Pro.local>
2016-12-03 16:08:23 +0900

    キャッシュ削除

--- src/js/app/app.js
+++
@@ -1,7 +0,0 @@
-var accordion = require('../common/accordion');
-var headerTracking = require('../common/headerTracking');
-var rating = require('../common/rating');
-
-new accordion();
-new headerTracking();
-new rating();
