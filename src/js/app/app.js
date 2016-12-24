8460e5e0d09348a8e4a505ddc19c0dc05f2626c7
sakana-san <sakana@uozakimasaru-no-MacBook-Pro.local>
2016-12-24 15:28:39 +0900

    いらないコードを削除した

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,7 +1,5 @@
 var accordion = require('../common/accordion');
 var headerTracking = require('../common/headerTracking');
-var rating = require('../common/rating');
 
 new accordion();
 new headerTracking();
-new rating();
