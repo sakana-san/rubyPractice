dbcf691ef5db9a8e51ea625aea1be7121931fa42
sakana-san <sakana@uozakimasaru-no-MacBook-Pro.local>
2016-12-25 18:42:19 +0900

    コメントを修正した

--- conf_erb.rb
+++ conf_erb.rb
@@ -33,10 +33,13 @@ server.config[:MimeTypes]["erb"] = "text/html"
 # -------------------------------------------------------------------------------------
 # ブランチが複雑なので一旦説明します
 
-# ●feature/ruby_production_application 選手データの完成品が入っている。今実装には関係ないのでいじらない
-# ●feature/ruby_production_application_dev 打ち上げ花火の完成品が入っている。基本的にはここをいじらない
-# ●feature/ruby_production_application_dev_mounting 練習用の何も入っていないdefaultのブランチ。ここにいろいろ入れていく
-# ●feature/ruby_production_application_dev_mounting_list listページを作るブランチ。作り終えたら、ruby_production_application_dev_mounting
+# ●feature/ruby_studyLesson       選手データのtestDbi.rbが入っている。ここからdatabaseを作成している
+# ●feature/ruby_studyLesson_dev   花火データのtestDbi.rbが入っている。ここからdatabaseを作成している
+
+# ●feature/ruby_production_application                  選手データの完成品が入っている。今実装には関係ないのでいじらない
+# ●feature/ruby_production_application_dev              打ち上げ花火の完成品が入っている。基本的にはここをいじらない
+# ●feature/ruby_production_application_dev_mounting     練習用の何も入っていないdefaultのブランチ。ここにいろいろ入れていく
+# ●feature/ruby_production_application_dev_mounting_○◯  ○◯ページを作るブランチ。作り終えたら、ruby_production_application_dev_mounting
 #  にmergeする
 
 # 基本的には1ページずつブランチを作って、feature/ruby_production_application_dev_mountingにmergeしていく
